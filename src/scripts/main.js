4334472704510c8bba22d1075725251dec384b67
carlosebl <carlosebl@gmail.com>
2024-02-09 13:14:04 -0300

    ajustada responsividade

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('olá mundo');
+const menu = document.querySelector(".header__mobile__menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".header__mobile__top__menu__hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+    if (menu.classList.contains("showMenu")) {
+        menu.classList.remove("showMenu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+    } else {
+        menu.classList.add("showMenu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+    }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+    function(menuItem) { 
+        menuItem.addEventListener("click", toggleMenu);
+    }
+)
